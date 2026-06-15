45d7c20a0028de2ce40d57202c37411e211159bb
ltopic764 <ltopic764@gmail.com>
2026-06-15 08:24:40 +0200

    Phase 3: Create Hough accumulator

--- HoughTransformation/HoughTransformation/Hough.cpp
+++ HoughTransformation/HoughTransformation/Hough.cpp
@@ -1,18 +1,73 @@
 #include "hough.h"
 #include <oneapi/tbb/parallel_for.h>
 #include <oneapi/tbb/blocked_range.h>
+#include <oneapi/tbb/blocked_range2d.h>
 #include <oneapi/tbb/concurrent_vector.h>
 #include <atomic>
 #include <cmath>
+#include <iostream>
+
+const double PI = 3.14159265358979323846;
 
 // Phase 3
 // Hough accumulator
 std::vector<std::vector<std::atomic<int>>> buildAccumulator(const Image& edges, int numTheta, int numRho, double& rhoMax) {
 
-	// TODO: initialize accumulator with atomis ints set to 0
-	// TODO: implement with parallel_for over rows of esge image
+	if (edges.channels != 1) {
+		throw std::runtime_error("buildAccumulator expects an image where cannels = 1");
+	}
 
-	return {};
+	// Calculate rhoMax = image diagonal
+	rhoMax = std::sqrt(
+		static_cast<double>(edges.width) * edges.width +
+		static_cast<double>(edges.height) * edges.height
+	);
+
+	// Create accumulator
+	std::vector<std::vector<std::atomic<int>>> accumulator(numRho);
+	for (int r = 0; r < numRho; ++r) {
+		// initialize all values to 0
+		accumulator[r] = std::vector<std::atomic<int>>(numTheta);
+		for (int t = 0; t < numTheta; ++t) {
+			accumulator[r][t].store(0);
+		}
+	}
+
+	double thetaStep = PI / numTheta;
+
+	oneapi::tbb::parallel_for(oneapi::tbb::blocked_range<int>(0, edges.height), [&](const oneapi::tbb::blocked_range<int>& range) {
+		for (int y = range.begin(); y < range.end(); ++y) {
+			for (int x = 0; x < edges.width; ++x) {
+
+				int idx = y * edges.width + x;
+
+				// if not edges node, continue
+				if (edges.data[idx] != 255) {
+					continue;
+				}
+
+				for (int t = 0; t < numTheta; ++t) {
+
+					double theta = t * thetaStep;
+
+					// Houghs formula
+					double rho = x * std::cos(theta) + y * std::sin(theta);
+
+					int rhoIdx = static_cast<int>(
+						((rho + rhoMax) / (2.0 * rhoMax)) * numRho
+						);
+
+					if (rhoIdx < 0) rhoIdx = 0;
+					if (rhoIdx >= numRho) rhoIdx = numRho - 1;
+
+					accumulator[rhoIdx][t].fetch_add(1);
+				}
+			}
+		}
+		}
+	);
+
+	return accumulator;
 }
 
 // Phase 4
